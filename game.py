20dd98669b8677b4f5e7906d51ae91429182619f
Victor Carmona <victor181851@gmail.com>
2023-06-14 23:14:59 -0600

    feat: It was created a Player class
    The player class was created, with its respective constructor, this one with the attributes name, weapons and health.

--- game.py
+++ game.py
@@ -5,4 +5,10 @@ class Weapon:
 
     def __repr__(self):
         return self.name
-    
+
+
+class Player:
+    def __init__(self, name, weapons, health):
+        self.name = name
+        self.weapons = weapons
+        self.health = health
