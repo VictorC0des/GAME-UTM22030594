58b1c93e53b9a1de14e38e688b82c6d1de64eb22
Victor Carmona <victor181851@gmail.com>
2023-06-14 23:21:26 -0600

    feat: added select_weapon() and attack() methods
    Implemented the select_weapon() method in the Player class to allow players to choose their weapons during battles. Also, added attack() method to allow players to attack their opponents using the selected weapon.

--- game.py
+++ game.py
@@ -12,3 +12,19 @@ class Player:
         self.name = name
         self.weapons = weapons
         self.health = health
+
+    def select_weapon(self):
+        # Method for the player to select a weapon
+        print(f"{self.name}, select your weapon:")
+        for i, weapon in enumerate(self.weapons):
+            print(f"{i + 1}. {weapon}")
+        choice = int(input("Enter the number of your weapon: ")) - 1
+        return self.weapons[choice]
+
+    def attack(self, opponent):
+        # Method for the player to attack their opponent
+        weapon = self.select_weapon()
+        print(f"{self.name} attacks with {weapon.name}!")
+        damage = weapon.damage
+        print(f"{opponent.name} takes {damage} damage!")
+        opponent.lose_health(damage)
